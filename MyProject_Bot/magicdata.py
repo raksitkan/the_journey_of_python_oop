b8e622cfdb851f473c92ab21c30a46a0c31cdd4a
raksitkandev0072 <raksitkan0072@gmail.com>
2024-12-14 17:18:44 +0700

    remove
    ลบไฟล์ที่ไม่เกี่ยวข้อง

--- MyProject_Bot/magicdata.py
+++
@@ -1,8 +0,0 @@
-magicdata = [
-    {'loc': (52+20, 757), 'key': 'A'},
-    {'loc': (165+30,757), 'key': 'S'},
-    {'loc': (278+60,757), 'key': 'D'},
-    {'loc': (388+90,757), 'key': 'F'}
-]
-# for loc in magicdata:
-#     print(loc['output'])
